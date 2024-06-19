eecc1de67b892c443fafd18c7ea711edf572d05f
Ulric Wilfred <shaman.sir@gmail.com>
2024-06-20 01:08:57 +0200

    prototyping application state

--- src/Main.purs
+++ src/Main.purs
@@ -6,10 +6,25 @@ import Effect (Effect)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
 import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
 
+import Data.Maybe (Maybe(..))
+
+import Grammar (Grammar)
 import Grammar.AST (AST)
+
+
+type State =
+  { input :: String
+  , grammarInput :: String
+  , grammar :: Maybe Grammar
+  }
+
+
+type Input =
+  { }
 
 
 main :: Effect Unit
@@ -17,24 +32,35 @@ main = HA.runHalogenAff do
   body <- HA.awaitBody
   runUI component unit body
 
+
+initialState :: State
+initialState =
+  { input : ""
+  , grammarInput : ""
+  , grammar : Nothing
+  }
+
+
 data Action = Increment | Decrement
 
+
+component :: forall query input output m. H.Component query input output m
 component =
   H.mkComponent
-    { initialState
+    { initialState : const initialState
     , render
     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
   where
-  initialState _ = 0
 
   render state =
     HH.div_
       [ HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
       , HH.div_ [ HH.text $ show state ]
       , HH.button [ HE.onClick \_ -> Increment ] [ HH.text "+" ]
+      , HH.textarea [ HE.onInput \_ -> Decrement, HE.onValueChange \_ -> Increment ]
       ]
 
   handleAction = case _ of
-    Increment -> H.modify_ \state -> state + 1
-    Decrement -> H.modify_ \state -> state - 1
+    Increment -> H.modify_ \state -> state { input = state.input <> "a" }
+    Decrement -> H.modify_ \state -> state { input = state.input <> "b" }
